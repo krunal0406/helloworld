b065ed75ebeb3e70e484b3a6591703284e5c9e15
Krunal <krunal.0406@gmail.com>
2021-09-16 07:04:52 +0530

    revert fourth

--- Weather/Weather/ContentView.swift
+++ Weather/Weather/ContentView.swift
@@ -25,7 +25,7 @@ struct ContentView: View {
                 .padding()
             
             Text(self.weatherVM.temprature)
-                .font(.custom("Arial", size: 110))
+                .font(.custom("Arial", size: 100))
                 .foregroundColor(.white)
                 .offset(y:100)
                 .padding()
